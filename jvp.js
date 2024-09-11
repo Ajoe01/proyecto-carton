521a6b39faef8f9103ed904f2ab2e7aa569515b5
Ajoe01 <139839835+Ajoe01@users.noreply.github.com>
2024-09-10 19:35:51 -0500

    Delete jvp.js

--- jvp.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const schoolName = document.getElementById('school-name').value;
-    const email = document.getElementById('email').value;
-    alert(`Gracias por inscribirse, ${schoolName}! Nos pondremos en contacto a través del correo ${email}.`);
-});
